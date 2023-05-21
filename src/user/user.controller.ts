551fa9ee8826a3b159cc14d55e900347823f7ff5
nazaret <nazaret155@mail.ru>
2023-05-21 13:25:35 +0300

    add get all users method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,17 +4,26 @@ import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
 import { EditUserDto } from './dto';
 import { UserService } from './user.service';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
-@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @ApiBearerAuth()
+  @UseGuards(JwtGuard)
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
   }
 
+  @Get()
+  getUsers() {
+    return this.userService.getUsers();
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(JwtGuard)
   @Patch()
   edituser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
     return this.userService.editUser(userId, dto);
